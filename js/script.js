48fe9420b46d5e9fc87e62d25294cdc2726bb31d
Максим Чупров <63455442+foxxman@users.noreply.github.com>
2021-01-15 21:55:21 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const buttonOpen = document.querySelector('.open');
 const headerNav = document.querySelector('.header__nav');
-
+console.log('11111');
 buttonOpen.addEventListener('click',()=>{
     buttonOpen.classList.toggle('open-active');
     headerNav.classList.toggle('header__nav-active');
