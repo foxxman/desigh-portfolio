262723525c4a43c10f1037e72faadb06d3b48f0d
Максим Чупров <63455442+foxxman@users.noreply.github.com>
2021-01-19 02:15:47 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,49 @@
-"use script";
 
-const buttonOpen = document.querySelector(".open");
-const headerNav = document.querySelector(".header__nav");
-const pointDescr = document.getElementsByClassName(
-  "services__point__descriotion"
-);
-const pointTextMobile = [
-  "Разработка дизайна в графическом редакторе с учетом пожеланий клиента",
-  "Разработка дизайна с последующей вёрсткой на Tilda ",
-  "Дизайн разрабатывается с учётом фирменного стиля компании",
-];
-console.log(window.innerWidth);
+$(document).ready(function () {
 
-buttonOpen.addEventListener("click", () => {
-  buttonOpen.classList.toggle("open-active");
-  headerNav.classList.toggle("header__nav-active");
+  $("a.buttonLink").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top-100 + "px",
+      },
+      {
+        duration: 500,
+        easing: "swing",
+      }
+    );
+    return false;
+  });
+  $("a.header__nav__item").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top-130 + "px",
+      },
+      {
+        duration: 500,
+        easing: "swing",
+      }
+    );
+    buttonOpen.classList.remove("open-active");
+    headerNav.classList.remove("header__nav-active");
+    return false;
+  });
+  const buttonOpen = document.querySelector(".open");
+  const headerNav = document.querySelector(".header__nav");
+  const pointDescr = document.getElementsByClassName(
+    "services__point__descriotion"
+  );
+  const pointTextMobile = [
+    "Разработка дизайна в графическом редакторе с учетом пожеланий клиента",
+    "Разработка дизайна с последующей вёрсткой на Tilda ",
+    "Дизайн разрабатывается с учётом фирменного стиля компании",
+  ];
+  console.log(window.innerWidth);
+
+  buttonOpen.addEventListener("click", () => {
+    buttonOpen.classList.toggle("open-active");
+    headerNav.classList.toggle("header__nav-active");
+  });
+
+  if (window.innerWidth < 610)
+    for (let i = 0; i < 3; i++) pointDescr[i].innerHTML = pointTextMobile[i];
 });
-
-if (window.innerWidth < 610)
-  for (let i = 0; i < 3; i++) pointDescr[i].innerHTML = pointTextMobile[i];
