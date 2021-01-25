91ae7b71bd9b77bd66d76ec28831377e0b35edf2
Максим Чупров <63455442+foxxman@users.noreply.github.com>
2021-01-25 13:10:16 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,53 @@
-"use script";
-new WOW().init();
+const buttonOpen = document.querySelector(".open");
+const headerNav = document.querySelector(".header__nav");
+const pointDescr = document.getElementsByClassName(
+  "services__point__descriotion"
+);
+const pointTextMobile = [
+  "Разработка дизайна в графическом редакторе с учетом пожеланий клиента",
+  "Разработка дизайна с последующей вёрсткой на Tilda ",
+  "Дизайн разрабатывается с учётом фирменного стиля компании",
+];
 
-const openButton = document.querySelector('.open-menu');
-const navMini = document.querySelector('.header__nav-mini');
-const body = document.querySelector('body');
-const closeButton = document.querySelector('.close');
-const line_2 = document.querySelector('.line-2');
-const line_5 = document.querySelector('.line-5');
-const line_8 = document.querySelector('.line-8');
-const cells = document.querySelectorAll('.works__cell');
-const worksTitle =document.querySelector('.works__title ');
+$(document).ready(function () {
+  new WOW().init();
+  //прокрутка до формы
+  $("a.buttonLink").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100 + "px",
+      },
+      {
+        duration: 500,
+        easing: "swing",
+      }
+    );
+    return false;
+  });
+  //прокрутка до раздела
+  $("a.header__nav__item").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 130 + "px",
+      },
+      {
+        duration: 500,
+        easing: "swing",
+      }
+    );
+    buttonOpen.classList.remove("open-active");
+    headerNav.classList.remove("header__nav-active");
+    return false;
+  });
 
+  // console.log(window.innerWidth);
+//открытие окна навигации на мобилках
+  buttonOpen.addEventListener("click", () => {
+    buttonOpen.classList.toggle("open-active");
+    headerNav.classList.toggle("header__nav-active");
+    // "animate__animated",'animate__fadeInRight'
+  });
 
-
-if(window.screen.width<570){
-    line_2.classList.remove('wow');
-    line_5.classList.remove('wow');
-    line_8.classList.remove('wow');
-
-    worksTitle.classList.remove('wow', 'animate__animated');
-    cells.forEach(item =>{
-        item.classList.remove('wow','animate__animated');
-    })
-}
-
-openButton.addEventListener('click',()=>{
-    navMini.classList.remove('animate__fadeOutRightBig');
-    navMini.classList.add('animate__fadeInRightBig')
-    navMini.style = "display:block;";
-    body.style = "overflow: hidden;";
-})
-
-closeButton.addEventListener('click',()=>{
-    navMini.classList.remove('animate__fadeInRightBig');
-    navMini.classList.add('animate__fadeOutRightBig');
-    // navMini.style = "display:none;";
-    body.style = "overflow: visible;";
-})
+  if (window.innerWidth < 610)
+    for (let i = 0; i < 3; i++) pointDescr[i].innerHTML = pointTextMobile[i];
+});
